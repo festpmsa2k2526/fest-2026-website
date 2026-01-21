df46525665abc9502b9faacfd1141b91abcb8128
msalihkc <codewithsalih@gmail.com>
2026-01-21 13:01:53 +0530

    SONG

--- app/screen/page.tsx
+++ app/screen/page.tsx
@@ -111,10 +111,10 @@ export default function StagePage() {
           className="text-center z-20 fixed bottom-[10dvh] text-white px-4"
         >
           <h1 className="text-7xl font-montserat font-bold uppercase mb-2 text-white">
-            TED X
+            SONG MALAYALAM
           </h1>
           <p className="text-4xl font-montserat uppercase text-white">
-            GENERAL
+            SENIOR
           </p>
         </motion.div>
       </div>
